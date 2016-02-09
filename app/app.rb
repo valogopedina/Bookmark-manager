578a7e34b6dff0b2ffc95c49b6674baffe4ff577
valogopedina <valentina.monetta@tiscali.it>
2016-02-09 17:31:19 +0000

    Create sinatra app

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,15 @@
 require 'sinatra/base'
-
+require 'rubygems'
+require 'data_mapper'
+require_relative './models/link'
 
 
 class BookmarkManager < Sinatra::Base
 
-  get '/' do
-    'list of links'
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+
   end
 
   # start the server if ruby file executed directly

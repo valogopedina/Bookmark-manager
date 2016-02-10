22612b5a330b4753549265c75a151420b6a68c43
valogopedina <valentina.monetta@tiscali.it>
2016-02-10 12:14:03 +0000

    Implemented to add new link

--- app/app.rb
+++ app/app.rb
@@ -9,7 +9,15 @@ class BookmarkManager < Sinatra::Base
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
 
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url],title: params[:title])
+    redirect '/links'
   end
 
   # start the server if ruby file executed directly
